bf5752ac27856e5f9055ae4e787495af86c88a91
RIERI <52834702+angeloecon@users.noreply.github.com>
2024-12-03 02:00:53 +0800

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const sections = document.querySelectorAll('section');
+
 const navLinks = document.querySelectorAll('.nav-link');
 const contents = document.querySelectorAll('.summary-content');
 const closeSvg = document.querySelector('.close-svg');
@@ -10,16 +11,16 @@ let slides = document.getElementsByClassName("mySlides");
 let dots = document.getElementsByClassName("dot");
 let lastScrollTop = 0;
 
-sidebar.addEventListener('change', function() {
+sidebar.addEventListener('change', function () {
   if (this.checked) {
-      // console.log('Sidebar is open');
-      openSvg.style.transition = "0.7s";
-      openSvg.style.rotate = "180deg";
-      openSvg.style.fill = "red";
+    // console.log('Sidebar is open');
+    openSvg.style.transition = "0.7s";
+    openSvg.style.rotate = "180deg";
+    openSvg.style.fill = "red";
   } else {
-      // console.log('Sidebar is closed');
-      openSvg.style.rotate = "0deg";
-      openSvg.style.fill = "white";
+    // console.log('Sidebar is closed');
+    openSvg.style.rotate = "0deg";
+    openSvg.style.fill = "white";
   }
 });
 
@@ -37,11 +38,11 @@ window.addEventListener('scroll', () => {
     }
   }
 
-  if(currentScrollTop >= 730){
-    vidPause.pause();
+  if (currentScrollTop >= 730) {
+    // vidPause.pause();
     welcome.style.display = "none";
   } else {
-    vidPause.play();
+    // vidPause.play();
     welcome.style.display = "block";
   }
 
@@ -54,7 +55,7 @@ window.addEventListener('scroll', () => {
   sections.forEach(section => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
- 
+
     if (window.scrollY >= sectionTop - sectionHeight / 3) {
       current = section.getAttribute('id');
     }
@@ -70,11 +71,11 @@ window.addEventListener('scroll', () => {
 
 function toggleContent(index) {
   contents.forEach((content, i) => {
-      if (i === index) {
-          content.style.display = content.style.display === 'block' ? 'none' : 'block';
-      } else {
-          content.style.display = 'none'; 
-      }
+    if (i === index) {
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    } else {
+      content.style.display = 'none';
+    }
   });
 }
 
@@ -92,14 +93,61 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
 
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+
+  const json = JSON.stringify(object);
+  console.log(formData);
+  console.log(object);
+  console.log(json);
+  result.innerHTML = "Please wait..."
+
+  fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = "Form submitted successfully";
+
+        result.classList.add("success");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+      result.classList.add("failed");
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 3000);
+    });
+});
+
